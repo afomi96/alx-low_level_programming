30e2735b533b7c4aae05aba3812bc88d9787d6ad
afomi96 <bamlakzewdie@gmail.com>
2022-12-08 15:00:43 -0800

    slash

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
-
 #include <unisted.h>
 
 /**
- *
- *  * main - Entry
- *
- *   * Return: 1
- *
- *    */
-
+ * main - Entry
+ * Return: 1
+ */
 int main(void)
-
-{ 
-
-	       write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-	               return (1);
-
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
-
-

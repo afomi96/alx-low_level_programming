1b88b601510cc8288401480f096a81eb006f6eab
afomi96 <bamlakzewdie@gmail.com>
2022-12-09 18:24:47 -0800

    print number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
  /**
- * main - Entry point
+ * main - Prints all possible combinations of two two-digit numbers,
+ *		ranging from 0-99, separated by a comma followed by a space.
  *
- * Description: 'print numbers'
+ * Description: main
  *
  * Return: Always 0.
+ *
  */
 
 int main(void)

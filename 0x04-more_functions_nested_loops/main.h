ffa5e917f646b4c3dc0696a4d644d0f3b4f054eb
afomi96 <bamlakzewdie@gmail.com>
2022-12-16 05:01:49 -0800

    am trying

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -14,4 +14,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif 
+#endif /* MAIN_H */

ca1edb8d1c6116fe8c30795cecfeac3a0caabab4
afomi96 <bamlakzewdie@gmail.com>
2022-12-19 13:46:26 -0800

    work

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,8 +5,28 @@
  * @s: input
  * Return: Always 0 (Success)
  */
+
 int _atoi(char *s)
 {
+	int sign;
+	unsigned int num;
+	char *temp;
 
-	return (0);
+	temp = s;
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			sign *= -1;
+		temp++;
+	}
+		if (*temp != '\0')
+	{
+			do {
+				num = num * 10 + (*temp - '0');
+				temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
 }

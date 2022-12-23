fd6a302435de82d3d40c40e8f819db215803566f
afomi96 <bamlakzewdie@gmail.com>
2022-12-23 12:24:24 -0800

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,10 +12,10 @@ char *cap_string(char *z)
 	int i = 0;
 	int j;
 	char badBoys[] = " \n\t,;.!?\"(){}";
-	
+
 	if (z[0] >= 'a' && z[0] <= 'z')
 		z[0] -= 32;
-	
+
 	for (; z[i] != '\0'; i++)
 	{
 		for (j = 0; j < 14; j++)
